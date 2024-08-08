7ebbe247a2dcce77a9313e9bfb45c92210a88723
amit <amit@gmail.com>
2024-08-08 10:22:31 +0000

    i am adding egress method in resource.tf

--- resource.tf
+++ resource.tf
@@ -18,18 +18,29 @@ resource "aws_security_group" "web_sg" {
     for_each = var.ports
     iterator = port
     content {
+
       from_port   = port.value
       to_port     = port.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
+
 resource "aws_instance" "web" {
-  ami           = var.aws_ami
-  instance_type = var.aws_instance_type
-  key_name      = aws_key_pair.deployer.key_name
+  ami             = var.aws_ami
+  instance_type   = var.aws_instance_type
+  key_name        = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = ["${aws_security_group.web_sg.id}"]
 
   tags = {
     Name = "shreyas_terraform_create"
